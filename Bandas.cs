16c7ede6cd11e96365ac622fa350514d9f04e6c0
Fernando <fernandozaragoza@live.com>
2023-05-01 18:21:43 -0600

    Agregar archivos de proyecto.

---
+++ Bandas.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorElectronica
+{
+    class Bandas
+    {
+        private int valor;
+        private System.Drawing.Color color { get; set; }
+        private double multiplicador;
+        private double tolerancia { get; set; }
+
+        public Bandas()
+        {
+        }
+
+        public Bandas(int valor,System.Drawing.Color color, double multiplicador, double tolerancia)
+        {
+            this.valor = valor;
+            this.color = color;
+            this.multiplicador = multiplicador;
+            this.tolerancia = tolerancia;
+        }
+
+        public void SetValor(int valor) {
+            this.valor = valor;
+        }
+
+        public int GetValor()
+        {
+            return this.valor;
+        }
+        public void SetColor(System.Drawing.Color color) {
+            this.color = color;
+        }
+        public System.Drawing.Color GetColor()
+        {
+            return this.color;
+        }
+
+        public void SetMultiplicador(double multiplicador)
+        {
+            this.multiplicador = multiplicador;
+        }
+        public double GetMultiplicador()
+        {
+            return this.multiplicador;
+        }
+
+        public void SetTolerancia(double tolerancia)
+        {
+            this.tolerancia = tolerancia;
+        }
+        public double GetTolerancia()
+        {
+            return this.tolerancia;
+        }
+        public override String ToString()
+        {
+            return "Color: " + color + "\nValor: " + valor + "\nMultiplicador: " + multiplicador + "\nTolerancia: " + tolerancia;
+        }
+    }
+
+}
---
+++ ColorChooserForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimuladorElectronica
+{
+    public partial class ColorChooserForm : Form
+    {
+        public ColorChooserForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
